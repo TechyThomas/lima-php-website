de7277d8688f4bf1121966e53392450631106479
Thomas Carter <thomas@techythomas.com>
2026-05-12 22:26:54 +0100

    Added overview link to docs sidebar

--- static/views/docs/_templates/sidebar.php
+++ static/views/docs/_templates/sidebar.php
@@ -3,8 +3,14 @@
         <div class="docs-sidebar__titlebar"><?php echo lucide_icon('file-text'); ?><span>Documentation</span></div>
         <div class="docs-sidebar__content">
             <nav aria-label="Documentation pages">
+                <a class="docs-nav-link <?php if (empty($data['current_doc']))
+                    echo 'is-active'; ?>" href="/docs">
+                    <span>Overview</span>
+                    <?php echo lucide_icon('chevron-right'); ?>
+                </a>
                 <?php foreach ($data['docs'] as $docSlug => $doc): ?>
-                    <a class="docs-nav-link <?php if (!empty($data['current_doc']) && $docSlug == $data['current_doc']) echo 'is-active'; ?>" href="/docs/<?php echo $docSlug; ?>">
+                    <a class="docs-nav-link <?php if (!empty($data['current_doc']) && $docSlug == $data['current_doc'])
+                        echo 'is-active'; ?>" href="/docs/<?php echo $docSlug; ?>">
                         <span><?php echo $doc['title']; ?></span>
                         <?php echo lucide_icon('chevron-right'); ?>
                     </a>
